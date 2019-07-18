3552d520f3b74aecf1388fa694548b0c6d7c4f4f
Matti Aho <ookooko@gmail.com>
2019-07-18 10:07:42 +0300

    forms and buttons - components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component'
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUpPage} />
     </div>
   );
 }
