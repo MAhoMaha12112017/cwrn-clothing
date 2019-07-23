8dd23cd2b876c0e7a6473983f7d3af1b8bfeefe5
Matti Aho <ookooko@gmail.com>
2019-07-23 16:56:53 +0300

    sign in handlesubmit method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,9 +14,32 @@ const config = {
 
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);  // console.log(firestore.collection('users').doc('434ssdfsdf'));
+  const snapShot = await userRef.get();
+  
+  if (!snapShot.exists) {
+    const {displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch(error) {
+      console.log('error creating data', error);
+    }
+  }
+  return userRef;
+}
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({prompt: 'select_account'});
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
